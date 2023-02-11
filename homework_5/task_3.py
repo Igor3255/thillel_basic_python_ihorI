59cf0c326237d14c03d5a0075efe1db85959e9b3
Igor3255 <122931874+Igor3255@users.noreply.github.com>
2023-02-11 22:25:28 +0200

    Add files via upload

---
+++ homework_5/task_3.py
@@ -0,0 +1,13 @@
+from math import pi
+
+
+def cone_square_and_volume(radius, height):  # returns 2 floats
+    s = pi * radius * (radius + (radius**2 + height**2)**0.5)  # total area
+    v = (pi * radius**2 * height) / 3
+    return s, v
+
+
+radius, height = int(input('Введите радиус')), int(input('Введите высоту'))
+s, v = cone_square_and_volume(radius, height)
+print('Площадь =', s)
+print('Объем =', v)
