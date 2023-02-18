2e613401335e66ca385614541aae374a582d4a81
Igor3255 <122931874+Igor3255@users.noreply.github.com>
2023-02-18 23:33:18 +0200

    Update task_2.py

--- homework_5/task_2.py
+++ homework_5/task_2.py
@@ -3,6 +3,7 @@ def triangle_square_and_perimeter(a, b):
     perimeter = a + b + c # знаходимо периметр
     square = (a * b) / 2 # знаходимо площу
     return square, perimeter
+
 a = float(input("Введіть довжину першого катета: "))
 b = float(input("Введіть довжину другого катета: "))
 
