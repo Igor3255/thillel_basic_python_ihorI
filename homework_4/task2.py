72e3a7d3e43bdbdece077f498cab7b65f845d211
Igor3255 <122931874+Igor3255@users.noreply.github.com>
2023-02-18 16:20:59 +0200

    Update task2.py

--- homework_4/task2.py
+++ homework_4/task2.py
@@ -1,6 +1,7 @@
-text = 'Taras Shevchenko*1814-03-09*1861-03-10'
+text = input("Введите информацию о человеке в формате 'имя*дата_рождения*дата_смерти' (например, 'Taras Shevchenko*1814-03-09*1861-03-10'): ")
 new_text = text.split('*')
 name = new_text[0]
 age = int(new_text[2][0:4]) - int(new_text[1][0:4])
 print('Name:', name)
 print('Age:', age, 'years')
+
