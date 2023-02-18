55926acca07f2530b08e714a606d7a9d94cddf26
Igor3255 <122931874+Igor3255@users.noreply.github.com>
2023-02-18 16:33:58 +0200

    Update task_1.py

--- homework_5/task_1.py
+++ homework_5/task_1.py
@@ -1,11 +1,12 @@
-from math import cos, pi
+import math
 
+def degrees2radians(degrees):
+    return degrees * 3.14159265359 / 180
 
-def degrees2radians(corner_degree):  # returns float: radians value
-    corner_radians = corner_degree * (pi / 180)
-    return corner_radians
+cos60 = math.cos(degrees2radians(60))
+cos45 = math.cos(degrees2radians(45))
+cos40 = math.cos(degrees2radians(40))
 
-
-print('cos 60 =', cos(degrees2radians(60)))
-print('cos 45 =', cos(degrees2radians(45)))
-print('cos 40 =', cos(degrees2radians(40)))
+print(f"cos(60) = {cos60}")
+print(f"cos(45) = {cos45}")
+print(f"cos(40) = {cos40}")
