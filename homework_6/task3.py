3857b457e798f8ad489acf3e7f0bf1d11f6362f6
Igor3255 <122931874+Igor3255@users.noreply.github.com>
2023-02-20 22:51:32 +0200

    Update task3.py

--- homework_6/task3.py
+++ homework_6/task3.py
@@ -5,12 +5,8 @@ def solve_quadratic_equation(a, b, c):
     d = b**2 - 4*a*c
     
     # розрахунок коренів рівняння
-    if d >= 0:
-        x1 = (-b + cmath.sqrt(d)) / (2*a)
-        x2 = (-b - cmath.sqrt(d)) / (2*a)
-    else:
-        x1 = (-b + cmath.sqrt(d)) / (2*a)
-        x2 = (-b - cmath.sqrt(d)) / (2*a)
+    x1 = (-b + cmath.sqrt(d)) / (2*a)
+    x2 = (-b - cmath.sqrt(d)) / (2*a)
     
     return x1, x2
 
@@ -19,10 +15,14 @@ a = float(input("Введіть коефіцієнт a: "))
 b = float(input("Введіть коефіцієнт b: "))
 c = float(input("Введіть коефіцієнт c: "))
 
-
 x1, x2 = solve_quadratic_equation(a, b, c)
-if x1 == x2:
-    print("x =", x1)
-else:
+
+if isinstance(x1, complex):
     print("x1 =", x1)
     print("x2 =", x2)
+else:
+    if x1 == x2:
+        print("x =", x1)
+    else:
+        print("x1 =", x1)
+        print("x2 =", x2)
