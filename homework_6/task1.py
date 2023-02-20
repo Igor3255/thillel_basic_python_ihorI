7f6b57660d0d5fdec9000fd8b4369cc167b5124f
Igor3255 <122931874+Igor3255@users.noreply.github.com>
2023-02-20 22:33:54 +0200

    Update task1.py

--- homework_6/task1.py
+++ homework_6/task1.py
@@ -1,13 +1,5 @@
 def is_even(number):
-    return number % 2 == 0 or number == 0
-
-
-def main():
-    if is_even(3):
-        print('is a double digit')
+    if number % 2 == 0:
+        return True
     else:
-        print('is not a double digit')
-
-
-if __name__ == '__main__':
-    main()
+        return False
