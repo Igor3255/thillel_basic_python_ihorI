6717b616b15f0d41add8a1034d2dc6a8fbdb90d5
Igor3255 <122931874+Igor3255@users.noreply.github.com>
2023-03-28 12:43:58 +0300

    Update task_3b.py

--- homework_8/task_3b.py
+++ homework_8/task_3b.py
@@ -1,6 +1,14 @@
-def sort_by_first_digit(lst):
-    return sorted(lst, key=lambda x: str(x)[0] if isinstance(x, str) and x.isdigit() else str(x))
+def sorted_list_value_first_digit(original_list):
+    return sorted(original_list, key=lambda item: str(item)[0])
 
-lst = ["105", "52", "88", "1", "93", "356", "67", "444", "0"]
-lst_sorted = sort_by_first_digit(lst)
-print(lst_sorted)  # выведет ["1", "105", "93", "0", "356", "52", "67", "88", "444"]
+
+def main():
+    original_list = [472, 326, 1, 999.0, '1101000', '99', 9, '20', 863, '0']
+    sorted_copy_list = sorted_list_value_first_digit(original_list)
+
+    print("Original list:", original_list)
+    print("Sorted copy list:", sorted_copy_list)
+
+
+if __name__ == "__main__":
+    main()
