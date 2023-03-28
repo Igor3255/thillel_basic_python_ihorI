09d704d04be671b51d38fcd079d36dc79d9fe5f6
Igor3255 <122931874+Igor3255@users.noreply.github.com>
2023-03-28 12:47:24 +0300

    Update task_3.py

--- homework_10/task_3.py
+++ homework_10/task_3.py
@@ -1,8 +1,6 @@
-from random import sample
-
-
 def pemrtuate(text):
     words = text.split()
+    
     
     for i in range(len(words)):
         if len(words[i]) > 3:
@@ -10,29 +8,16 @@ def pemrtuate(text):
             new_middle = []
             
             for j in range(0, len(middle), 3):
-            
                 if j + 3 <= len(middle):
                     original_triple = middle[j:j + 3]
                     permuted_triple = sample(original_triple, 3)
                     while permuted_triple == original_triple:
                         permuted_triple = sample(original_triple, 3)
                     new_middle += permuted_triple
-                    
                 else:
                     new_middle += middle[j:]
-                    
-            permuted_word = "".join([first] + new_middle + [last])
-            words[i] = permuted_word
+            
+            new_word = "".join([first] + new_middle + [last])
+            words[i] = new_word
             
     return " ".join(words)
-
-
-def main():
-    text = "Не має значення в якому порядку розташовані букви в слові"
-    reordered_text = pemrtuate(text)
-    print(reordered_text)
-
-
-if __name__ == "__main__":
-    main()
-    
